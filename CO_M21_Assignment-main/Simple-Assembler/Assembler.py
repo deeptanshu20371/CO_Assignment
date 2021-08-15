8f5aa82163094f8c2a76744700360459e4efd406
deeptanshu20371 <deeptanshu20371@iiitd.ac.in>
2021-08-15 19:52:36 +0530

    final 1
    yeet

--- CO_M21_Assignment-main/Simple-Assembler/Assembler.py
+++ CO_M21_Assignment-main/Simple-Assembler/Assembler.py
@@ -10,6 +10,12 @@ for x in range(len(lines)):
     l=line.split();
     instruction.append(l)
 
+instruction_copy=instruction[:]
+
+#Removing blank instructions
+while [] in instruction:
+    instruction.remove([])
+
 #Giving labels to binary values
 #Appends binary values of the label to instruction list
 #['loop:','add', 'R1', 'R1', 'R2'] => ['loop:','add', 'R1', 'R1', 'R2','00000000']
@@ -77,6 +83,108 @@ Reg_Address={
 
 #=========================Error_Handling=========================
 
+#Variable not declared at beginning(g)    
+total_variables=0;
+valid_variable_count=0;
+
+for i in instruction:
+    opcodeindex=0;
+    if(i[opcodeindex][-1]==':'):
+        opcodeindex=1;
+    if(i[opcodeindex]=='var'):
+        total_variables+=1;
+
+
+for i in instruction:
+    opcodeindex=0;
+    if(i[opcodeindex][-1]==':'):
+        opcodeindex=1;
+    if(i[opcodeindex]=='var'):
+        valid_variable_count+=1;
+    else:
+        break
+if(valid_variable_count!=total_variables):
+    for i in range(len(instruction)):
+        opcodeindex=0;
+        if(instruction[i][opcodeindex][-1]==':'):
+            opcodeindex=1;
+        if(instruction[len(instruction)-i-1][opcodeindex]=='var'):
+            print("Error at line "+str(instruction_copy.index(instruction[len(instruction)-i-1])+1)+": ",end='')
+            print('Variables not declared at the beginning')
+            sys.exit();    
+
+# (j) ~ Wrong syntax used for instructions (For example, add instruction being used as a type B instruction )
+for i in instruction:
+    opcodeindex=0
+    if(i[opcodeindex][-1]==':'):
+        opcodeindex=1;
+    #A
+    if(i[opcodeindex]=="add" or i[opcodeindex]=="sub" or i[opcodeindex]=="mul" or i[opcodeindex]=="xor" or i[opcodeindex]=="or" or i[opcodeindex]=="and"):
+        if len(i[opcodeindex:])==4:
+            continue
+        elif(i[0][-1]==':'):
+            if len(i[opcodeindex:])==5:
+                continue
+        else:
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
+            print("Wrong syntax used for instructions")
+            sys.exit()
+    #B        
+    elif(i[opcodeindex]=="rs" or i[opcodeindex]=="ls" or (i[opcodeindex]=="mov") and (i[opcodeindex+2][0]=="$")):
+        if len(i[opcodeindex:])==3:
+            continue
+        elif(i[0][-1]==':'):
+            if len(i[opcodeindex:])==4:
+                continue
+        else:
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
+            print("Wrong syntax used for instructions")
+            sys.exit()
+    #C
+    elif(i[opcodeindex]=="mov" or i[opcodeindex]=="div" or i[opcodeindex]=="not" or i[opcodeindex]=="cmp"):
+        if len(i[opcodeindex:])==3:
+            continue
+        elif(i[0][-1]==':'):
+            if len(i[opcodeindex:])==4:
+                continue
+        else:
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
+            print("Wrong syntax used for instructions")
+            sys.exit()
+    #D
+    elif(i[opcodeindex]=='ld' or i[opcodeindex]=='st'):   #--------review for memory address
+        if len(i[opcodeindex:])==3:
+            continue
+        elif(i[0][-1]==':'):
+            if len(i[opcodeindex:])==4:
+                continue
+        else:
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
+            print("Wrong syntax used for instructions")
+            sys.exit()
+    #E
+    elif(i[opcodeindex]=='jgt' or i[opcodeindex]=='jmp' or i[opcodeindex]=='jlt' or   i[opcodeindex]=='je'):
+        if len(i[opcodeindex:])==2:
+            continue
+        elif(i[0][-1]==':'):
+            if len(i[opcodeindex:])==3:
+                continue
+        else:
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
+            print("Wrong syntax used for instructions")
+            sys.exit()
+    
+    #F
+    elif(i[opcodeindex]== 'hlt'):
+        if len(i[opcodeindex:])==1:
+            continue
+        elif(i[0][-1]==':'):
+            if len(i[opcodeindex:])==2:
+                continue
+        else:
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
+            print("Wrong syntax used for instructions")
+            sys.exit()
 #Typos in instruction name(a)
 for i in instruction:
     if(i[0]=='var'):
@@ -87,7 +195,7 @@ for i in instruction:
     if(i[opcodeindex]=='add' or i[opcodeindex]=='sub' or i[opcodeindex]=='mov' or i[opcodeindex]=='ld' or i[opcodeindex]=='st' or i[opcodeindex]=='mul' or i[opcodeindex]=='div' or i[opcodeindex]=='rs' or i[opcodeindex]=='ls' or i[opcodeindex]=='xor' or i[opcodeindex]=='or' or i[opcodeindex]=='and' or i[opcodeindex]=='not' or i[opcodeindex]=='cmp' or i[opcodeindex]=='jmp' or i[opcodeindex]=='jlt' or i[opcodeindex]=='jgt' or i[opcodeindex]=='je' or i[opcodeindex]=='hlt' ):
         continue
     else:
-        print("Error at line "+str(instruction.index(i))+": ",end='')
+        print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
         print(i[opcodeindex]+" is not a valid instruction name.")
         sys.exit()
 
@@ -95,7 +203,7 @@ for i in instruction:
 for i in instruction:
     opcodeindex=0;
     if(i[opcodeindex][-1]==':'):
-            opcodeindex=1;
+        opcodeindex=1;
     #===========ErrorA===========
     if(i[opcodeindex]=="add" or i[opcodeindex]=="sub" or i[opcodeindex]=="mul" or i[opcodeindex]=="xor" or i[opcodeindex]=="or" or i[opcodeindex]=="and"):
         Error_flag=True
@@ -104,7 +212,7 @@ for i in instruction:
                 Error_flag=False
                 break
         if Error_flag==True:
-            print("Error at line "+str(instruction.index(i))+": ",end='')
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
             print(i[opcodeindex+1]+" is not a valid register name.")
             sys.exit()
         Error_flag=True
@@ -113,7 +221,7 @@ for i in instruction:
                 Error_flag=False
                 break
         if Error_flag==True:
-            print("Error at line "+str(instruction.index(i))+": ",end='')
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
             print(i[opcodeindex+2]+" is not a valid register name.")
             sys.exit()
         Error_flag=True
@@ -122,7 +230,7 @@ for i in instruction:
                 Error_flag=False
                 break
         if Error_flag==True:
-            print("Error at line "+str(instruction.index(i))+": ",end='')
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
             print(i[opcodeindex+3]+" is not a valid register name.")
             sys.exit()
     #===========ErrorB===========
@@ -133,7 +241,7 @@ for i in instruction:
                 Error_flag=False
                 break
         if Error_flag==True:
-            print("Error at line "+str(instruction.index(i))+": ",end='')
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
             print(i[opcodeindex+1]+" is not a valid register name.")
             sys.exit()
     #===========ErrorC===========
@@ -144,7 +252,7 @@ for i in instruction:
                 Error_flag=False
                 break
         if Error_flag==True:
-            print("Error at line "+str(instruction.index(i))+": ",end='')
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
             print(i[opcodeindex+1]+" is not a valid register name.")
             sys.exit()
         Error_flag=True
@@ -153,13 +261,13 @@ for i in instruction:
                 Error_flag=False
                 break
         if Error_flag==True:
-            print("Error at line "+str(instruction.index(i))+": ",end='')
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
             print(i[opcodeindex+2]+" is not a valid register name.")
             sys.exit()
     #===========ErrorD===========
     elif(i[opcodeindex]=='ld' or i[opcodeindex]=='st'):
         if (i[opcodeindex+2] not in variable_list):
-            print("Error at line "+str(instruction.index(i))+": ",end='')
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
             print(i[opcodeindex+2]+" is an undefined variable")
             sys.exit()
         Error_flag=True
@@ -168,94 +276,51 @@ for i in instruction:
                 Error_flag=False
                 break
         if Error_flag==True:
-            print("Error at line "+str(instruction.index(i))+": ",end='')
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
             print(i[opcodeindex+1]+" is not a valid register name.")
             sys.exit()
     #===========ErrorE===========
     elif(i[opcodeindex]=='jgt' or i[opcodeindex]=='jmp' or i[opcodeindex]=='jlt' or   i[opcodeindex]=='je'):
         if (i[opcodeindex+1] not in label_list):
-            print("Error at line "+str(instruction.index(i))+": ",end='')
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
             print(i[opcodeindex+1]+" is and undefined label")
             sys.exit()
             
 # (e) ~ Illegal Immediate values (less than 0 or more than 255)
 for i in instruction:
     opcodeindex=0
+    if(i[opcodeindex][-1]==':'):
+        opcodeindex=1;
     if (i[opcodeindex]=="mov") and (i[opcodeindex+2][0]=="$"):
         imm_val = int((i[opcodeindex+2][1:(len(i[opcodeindex+2]))]))
         if (imm_val >= 0) and (imm_val <= 255):
             continue
         else:
-            print("Error at line "+str(instruction.index(i))+": ",end='')
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
             print("Illegal Immediate values (less than 0 or more than 255)")
             sys.exit()
 
 # (f) ~ Misuse of labels as variables or vice-versa  --------------REVIEW
 for i in instruction:
     opcodeindex=0
+    if(i[opcodeindex][-1]==':'):
+        opcodeindex=1;
     if i[opcodeindex]=='ld' or i[opcodeindex]=='st':
         if (i[opcodeindex+2][-1])==":":
-            print("Error at line "+str(instruction.index(i))+": ",end='')
+            print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
             print("Misuse of labels as variables")
             sys.exit()
         else:
             continue
-
-# (j) ~ Wrong syntax used for instructions (For example, add instruction being used as a type B instruction )
-for i in instruction:
-    opcodeindex=0
-    #A
-    if(i[opcodeindex]=="add" or i[opcodeindex]=="sub" or i[opcodeindex]=="mul" or i[opcodeindex]=="xor" or i[opcodeindex]=="or" or i[opcodeindex]=="and"):
-        if len(i[opcodeindex:])==4:
-            continue
-        else:
-            print("Wrong syntax used for instructions")
-            sys.exit()
-    #B        
-    elif(i[opcodeindex]=="rs" or i[opcodeindex]=="ls" or (i[opcodeindex]=="mov") and (i[opcodeindex+2][0]=="$")):
-        if len(i[opcodeindex:])==3:
-            continue
-        else:
-            print("Wrong syntax used for instructions")
-            sys.exit()
-    #C
-    elif(i[opcodeindex]=="mov" or i[opcodeindex]=="div" or i[opcodeindex]=="not" or i[opcodeindex]=="cmp"):
-        if len(i[opcodeindex:])==3:
-            continue
-        else:
-            print("Wrong syntax used for instructions")
-            sys.exit()
-    #D
-    elif(i[opcodeindex]=='ld' or i[opcodeindex]=='st'):   #--------review for memory address
-        if len(i[opcodeindex:])==3:
-            continue
-        else:
-            print("Wrong syntax used for instructions")
-            sys.exit()
-    #E
-    elif(i[opcodeindex]=='jgt' or i[opcodeindex]=='jmp' or i[opcodeindex]=='jlt' or   i[opcodeindex]=='je'):
-        if len(i[opcodeindex:])==2:
-            continue
-        else:
-            print("Wrong syntax used for instructions")
-            sys.exit()
-    
-    #F
-    elif(i[opcodeindex]== 'hlt'):
-        if len(i[opcodeindex:])==1:
-            continue
-        else:
-            print("Wrong syntax used for instructions")
-            sys.exit()
                 
 #Halt errors( h and i)        
 halt_check=0;
 for i in range(len(instruction)):
     opcodeindex=0;
     if(instruction[i][opcodeindex][-1]==':'):
-            opcodeindex=1;
+        opcodeindex=1;
     if(instruction[i][opcodeindex]=='hlt' and i!=(len(instruction)-1) ):
-        print("Error at line "+str(i)+": ",end='')
+        print("Error at line "+str(instruction_copy.index(instruction[i])+1)+": ",end='')
         print("hlt is not the last instruction")
         sys.exit();
     
@@ -267,36 +332,6 @@ if halt_check==0:
     print('Missing hlt instruction')
     sys.exit();      
     
-
-#Variable not declared at beginning(g)    
-total_variables=0;
-valid_variable_count=0;
-
-for i in instruction:
-    opcodeindex=0;
-    if(i[opcodeindex][-1]==':'):
-            opcodeindex=1;
-    if(i[opcodeindex]=='var'):
-        total_variables+=1;
-
-
-for i in instruction:
-    opcodeindex=0;
-    if(i[opcodeindex][-1]==':'):
-            opcodeindex=1;
-    if(i[opcodeindex]=='var'):
-        valid_variable_count+=1;
-    else:
-        break
-if(valid_variable_count!=total_variables):
-    for i in range(len(instruction)):
-        opcodeindex=0;
-        if(i[opcodeindex][-1]==':'):
-            opcodeindex=1;
-        if(instruction[len(instructions)-i-1][opcodeindex]=='var'):
-            print("Error at line "+str(i)+": ",end='')
-    print('Variables not declared at the beginning')
-    sys.exit();    
  
 #flag error(d)
 for i in instruction:
@@ -306,11 +341,11 @@ for i in instruction:
     for j in i:
         if j=='FLAGS':
             if (i[opcodeindex]!='mov'):
-                print("Error at line "+str(instruction.index(i))+": ",end='')
+                print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
                 print('Illegal Use of Flags')
                 sys.exit();
             elif (i[opcodeindex]=='mov')and (opcodeindex+2)!=i.index(j):
-                print("Error at line "+str(instruction.index(i))+": ",end='')
+                print("Error at line "+str(instruction_copy.index(i)+1)+": ",end='')
                 print('Illegal Use of Flags')
                 sys.exit();
 
@@ -320,7 +355,7 @@ for i in instruction:
 for i in instruction:
     opcodeindex=0;
     if(i[opcodeindex][-1]==':'):
-            opcodeindex=1;
+        opcodeindex=1;
 
     #TypeA
     if(i[opcodeindex]=="add" or i[opcodeindex]=="sub" or i[opcodeindex]=="mul" or i[opcodeindex]=="xor" or i[opcodeindex]=="or" or i[opcodeindex]=="and"):
